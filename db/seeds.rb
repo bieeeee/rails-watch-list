fd572e2f02b5eb1bf981a0f44533ed80424d6120
hanbyeol kwon <janviertrois@naver.com>
2022-11-10 12:26:32 -0500

    20 movies scraped and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+movies = URI.open(url).read
+movie = JSON.parse(movies)
+
+movie["results"].each do |m|
+  Movie.create!(
+    title: m["original_title"],
+    overview: m["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{m["poster_path"]}",
+    rating: m["vote_average"]
+  )
+end
