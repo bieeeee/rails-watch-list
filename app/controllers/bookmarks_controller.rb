2fb207bf332fda830df002f415af64cadef60fda
hanbyeol kwon <janviertrois@naver.com>
2022-11-10 16:19:56 -0500

    new list, new bookmark, show list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,25 @@
 class BookmarksController < ApplicationController
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    # raise
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.movie = Movie.find(params[:movie_id])
+    @bookmark.list = List.find(params[:bookmark][:list_id])
+    # raise
+    if @bookmark.save!
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:list_id, :comment)
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,33 @@
 class ListsController < ApplicationController
+  before_action :set_lists
+
+  def index
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new, status: 303
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_lists
+    @lists = List.all
+  end
 end
