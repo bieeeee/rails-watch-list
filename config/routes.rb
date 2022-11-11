eec5894fe5c58cb9e0c7b871b19ffb819ce1f597
hanbyeol kwon <janviertrois@naver.com>
2022-11-11 11:32:06 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines thec root path route ("/")
+
+
   # root "articles#index"
   root to: 'lists#index'
   resources :lists, except: :index
